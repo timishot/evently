9283aeea05ef6a2e4b736355ca44aef12580291d
timishot <timidotcom58@gmail.com>
2024-10-19 15:59:06 +0100

    fix

--- middleware.ts
+++ middleware.ts
@@ -4,12 +4,12 @@ export default authMiddleware({
     publicRoutes: [
         '/',
         '/events/:id',
-        '/api/webhooks/clerk',
         '/api/webhooks/test',
         '/api/webhooks/stripe',
         '/api/uploadthing'
     ],
     ignoredRoutes: [
+        '/api/webhooks/clerk',
         '/api/webhooks/stripe',
         '/api/uploadthing'
     ]
