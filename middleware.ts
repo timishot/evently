87ed1ab103c64aa857fc51dca3ac4c1fe9a501e1
timishot <timidotcom58@gmail.com>
2024-10-20 23:16:45 +0100

    fix

--- middleware.ts
+++ middleware.ts
@@ -10,6 +10,7 @@ export default authMiddleware({
         '/api/uploadthing'
     ],
     ignoredRoutes: [
+        '/api/webhooks/clerk',
         '/api/webhooks/stripe',
         '/api/uploadthing'
     ]
